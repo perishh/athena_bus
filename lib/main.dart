2c7df3aeccae8ec34aede071d1340f250aae5307
Peris <vlassis.periklis@gmail.com>
2026-05-04 14:20:25 +0300

    Added Edge-to-Edge

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:athena_bus/screen/main/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+const _edgeToEdgeOverlayStyle = SystemUiOverlayStyle(
+  statusBarColor: Colors.transparent,
+  systemNavigationBarColor: Colors.transparent,
+  systemNavigationBarDividerColor: Colors.transparent,
+  statusBarIconBrightness: Brightness.dark,
+  statusBarBrightness: Brightness.light,
+  systemNavigationBarIconBrightness: Brightness.dark,
+  systemNavigationBarContrastEnforced: false,
+);
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(_edgeToEdgeOverlayStyle);
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(ProviderScope(retry: (_, _) => null, child: const MainApp()));
 }
 
@@ -12,10 +26,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "Google Sans"),
-      home: MainScreen(),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: _edgeToEdgeOverlayStyle,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: "Google Sans"),
+        home: MainScreen(),
+      ),
     );
   }
 }
